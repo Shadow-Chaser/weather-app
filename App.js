f51467781396d83c84be314b03e642ffeeb3ff96
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-06-03 21:47:27 +0600

    updated

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 const API_KEY = "46a9246bebba16d42b36aac3fc3ba8af";
 
 
 export default function App() {
   const [weatherData, setWeatherData] = useState(null);
+
+  const getWeatherData = (cityName) => {
+    useEffect(() => {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`)
+        .then(res => res.json())
+        .then(data => setWeatherData(data))
+
+    }, [])
+  }
+
 
   return (
     <View style={styles.container}>
