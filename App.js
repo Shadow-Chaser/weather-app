1812e12b4b2a624cda5574db85475e968387ddb5
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-06-03 20:30:33 +0600

    updated

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const API_KEY = "46a9246bebba16d42b36aac3fc3ba8af";
+
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
   return (
     <View style={styles.container}>
       <Text>Ashab Is going to build a native app</Text>
